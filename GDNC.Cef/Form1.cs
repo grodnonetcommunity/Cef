419f3e94470b6d0ed40bf360ea12f4db1fbf98b6
Alexander Puzynia <werwolf.by@gmail.com>
2017-10-20 00:13:25 +0300

    feat: start application with small index.html loaded from custom scheme

--- GDNC.Cef/Form1.cs
+++ GDNC.Cef/Form1.cs
@@ -7,14 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CefSharp;
+using CefSharp.SchemeHandler;
+using CefSharp.WinForms;
 
 namespace GDNC.Cef
 {
     public partial class Form1 : Form
     {
+        private readonly ChromiumWebBrowser _browser;
+
         public Form1()
         {
             InitializeComponent();
+
+            var settings = new CefSettings();
+
+            settings.RegisterScheme(new CefCustomScheme
+            {
+                SchemeName = "dotnet",
+                SchemeHandlerFactory = new FolderSchemeHandlerFactory("dist", "dotnet")
+            });
+
+            CefSharp.Cef.Initialize(settings);
+
+            _browser = new ChromiumWebBrowser("dotnet://index.html")
+            {
+                Dock = DockStyle.Fill
+            };
+            Controls.Add(_browser);
         }
     }
 }
