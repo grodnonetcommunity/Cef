e4bcf9ecb5c4f3228966b190ab94b5e8cfaeac4f
Alexander Puzynia <werwolf.by@gmail.com>
2017-10-20 00:50:25 +0300

    feat: example with promise callbacks and exceptions

--- GDNC.Cef/CSharpBus.cs
+++ GDNC.Cef/CSharpBus.cs
@@ -1,5 +1,7 @@
-﻿using System.Threading;
+﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
+using CefSharp;
 
 namespace GDNC.Cef
 {
@@ -31,6 +33,31 @@ namespace GDNC.Cef
             Thread.Sleep(delay);
 
             return "result";
+        }
+
+        public void Throw(string error)
+        {
+            throw new Exception(error);
+        }
+
+        public async void AsyncDelay(string echo, int delay, bool success, IJavascriptCallback resolve, IJavascriptCallback reject)
+        {
+            await Task.Delay(delay);
+
+            if (success)
+            {
+                if (!resolve.IsDisposed)
+                {
+                    await resolve.ExecuteAsync(echo);
+                }
+            }
+            else
+            {
+                if (!reject.IsDisposed)
+                {
+                    await reject.ExecuteAsync("Error");
+                }
+            }
         }
     }
 }
